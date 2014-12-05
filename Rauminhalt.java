3721f96cf3c45bce373e6bab55af543083215b41
Matthias Mischek <mmischek@student.tgm.ac.at>
2014-12-05 23:47:47 +0100

    Verbesserungen Mischek
    Testen von Rauminhalt.java fertig
    Kommentare add
    kleine Fehler ausgebessert

--- Rauminhalt.java
+++ Rauminhalt.java
@@ -1,30 +1,94 @@
 package teamarbeit;
 
-
+/**
+ * 
+ * @author Matthias
+ * @version 1.0
+ */
 
 public class Rauminhalt {
-	
-	private double a,b,c,r,h;
+
+	private double a, b, c, r;
+
+	/**
+	 * Standart Konstruktor
+	 */
+	public Rauminhalt() {
+		this.a = 0;
+		this.b = 0;
+		this.c = 0;
+		this.r = 0;
+	}
+
+	/**
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @param r
+	 * @param h
+	 */
+	public Rauminhalt(double a, double b, double c, double r) {
+
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.r = r;
+
+	}
+
+	/**
+	 * Konstruktor fuer Pyramide und Quader
+	 * @param a
+	 * @param b
+	 * @param h
+	 */
+	public Rauminhalt(double a, double b, double c) {
+
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
 	
 
-	public double pyramide(){
-		return (a*a*h)/3;
+	/**
+	 * Konstruktor fuer Kegel
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @param r
+	 * @param h
+	 */
+	public Rauminhalt(double r, double c) {
+		this.r = r;
+		this.c = c;
 	}
-	
-	
-	public double kegel(){
-		return ((Math.PI*r*r)*h)/3;
-		
+
+
+	/**
+	 * Methode zum berechnen des Volumes einer Pyramide
+	 * @return Rauminhalt von Pyramide
+	 */
+	public double pyramide() {
+		return (a * b * c) / 3;
 	}
-	
-	public double quader(){
-		return (a*b*c);
-		
+
+	/**
+	 *  Methode zum berechnen des Volumes eines Kegels
+	 * @return Rauminhalt von Kegel
+	 */
+	public double kegel() {
+		return ((Math.PI * r * r) * c) / 3;
+
 	}
-	
-	public static void main(String[] args) {
-		
+
+	/**
+	 * Methode zum berechnen des Volumes eines Quaders
+	 * @return Rauminhalt von  Quader
+	 */
+	public double quader() {
+		return (a * b * c);
+
 	}
-	
-	
+
+
 }
--- Testen.java
+++ Testen.java
@@ -1,10 +1,36 @@
 package teamarbeit;
 
+/**
+ * 
+ * @author Matthias Mischek
+ * @version 1.0
+ */
 public class Testen {
 
+/**
+ * main-Methode
+ * @param args
+ */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/**
+		 * Testen der Klasse Rauminhalt
+		 */
+		//Pyramide Methode wird getestet
+		Rauminhalt pyramide = new Rauminhalt(5.8, 2.9, 3.5);
+		System.out.println("Pyramide: \na = 2.8 | b = 2.9 | h = 3.5");
+		System.out.println("Volumen: " + pyramide.pyramide()+"\n");
+		
+		//Kegel Methode wird getestet
+		Rauminhalt kegel = new Rauminhalt(8.7, 5.9);
+		System.out.println("Kegel: \nr = 8,7 | h = 5.9");
+		System.out.println("Volumen: " + kegel.kegel()+"\n");
+		
+		//Quader Methode wird getestet
+		Rauminhalt quader = new Rauminhalt(7.5, 5.6,3.2);
+		System.out.println("Quader: a = 7,5 | b = 5.6 | c = 3.2");
+		System.out.println("Volumen: " + quader.quader());
+		
+		
 	}
 
 }
